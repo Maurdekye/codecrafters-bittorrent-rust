4752276779787a8a09a468d324fa3f1b7b701995
Maurdekye <ncolaprete@gmail.com>
2023-12-02 01:52:53 -0500

    decode bencoded integer

--- src/decode.rs
+++ src/decode.rs
@@ -3,11 +3,17 @@ use regex::Regex;
 use serde_json::Value;
 
 pub fn decode_bencoded_value(raw_value: &str) -> Result<Value, BitTorrentError> {
-    let decode_re =
-        Regex::new(r"^(\d+):(.*)$").expect("Decode regex did not compile (for some reason??)");
+    let decode_string_re = Regex::new(r"^(\d+):(.*)$")
+        .expect("String decode regex did not compile (for some reason??)");
+    let decode_integer_re = Regex::new(r"^i(-?\d+)e$")
+        .expect("Integer decode regex did not compile (for some reason??)");
 
-    match decode_re.captures(raw_value) {
-        Some(captures) => {
+    match (
+        decode_string_re.captures(raw_value),
+        decode_integer_re.captures(raw_value),
+    ) {
+        // decode string
+        (Some(captures), _) => {
             let passed_length: usize = captures
                 .get(1)
                 .ok_or(bterror!("length specifier not present"))?
@@ -28,6 +34,19 @@ pub fn decode_bencoded_value(raw_value: &str) -> Result<Value, BitTorrentError> 
                 Ok(Value::String(content.to_string()))
             }
         }
-        None => Err(bterror!("Unhandled encoded value: {}", raw_value)),
+        
+        // decode integer
+        (_, Some(captures)) => {
+            let value: i64 = captures
+                .get(1)
+                .ok_or(bterror!("value not present"))?
+                .as_str()
+                .parse()
+                .map_err(|err| bterror!("Error parsing encoded integer: {}", err))?;
+            Ok(Value::Number(value.into()))
+        }
+
+        // unrecognized value
+        _ => Err(bterror!("Unhandled encoded value: {}", raw_value)),
     }
 }
